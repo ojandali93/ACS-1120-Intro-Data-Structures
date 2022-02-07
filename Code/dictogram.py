1f4a582fafec7ca8a894d29bdc2cc008d0491b2f
OmarJ <omarjandali93@gmail.com>
2022-02-06 20:19:14 -0800

    got all of the test passing for the dictogram

--- Code/dictogram.py
+++ Code/dictogram.py
@@ -19,12 +19,13 @@ class Dictogram(dict):
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
         # TODO: Increase word frequency by count
-        if word in self.keys():
+        if word in self:
             self[word] += count
+            self.tokens += count
         else:
-            self[word] = count 
-            self.types += count
-        self.tokens += count
+            self[word] = count
+            self.types += 1
+            self.tokens += count
         
 
     def frequency(self, word):
@@ -40,15 +41,12 @@ class Dictogram(dict):
         """Return a word from this histogram, randomly sampled by weighting
         each word's probability of being chosen by its observed frequency."""
         # TODO: Randomly choose a word based on its frequency in this histogram
-        count = 0
-        returned_word = ''
-        random_integer = random.randint(0, self.tokens)
-        for key in self.keys():
-            if count <= random_integer:
-                count += self[key]
-                returned_word = key
-            else:
-                return returned_word
+        counter = 0
+        random_integer = random.uniform(0, self.tokens)
+        for key, value in self.items():
+            counter += value
+            if counter >= random_integer:
+                return key
 
 
 
